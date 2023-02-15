42331a521cb28a19d569f96417c327978e2cd24b
WarlockGames12 <47526227+WarlockGames12@users.noreply.github.com>
2023-02-15 13:51:18 +0100

    Tried to make the AI better

--- HotelOfDeath/Assets/Scripts/Enemy/RandomMovement.cs
+++ HotelOfDeath/Assets/Scripts/Enemy/RandomMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine.SocialPlatforms;
 
 public class RandomMovement : MonoBehaviour
 {
-
-    [Header("Navmesh Settings: ")] 
+ [Header("Navmesh Settings: ")] 
     [SerializeField] private NavMeshAgent enemyAgent;
     [SerializeField] private float enemyRange;
     [SerializeField] private float enemyChaseRange;
@@ -17,7 +16,10 @@ public class RandomMovement : MonoBehaviour
 
     private bool isChasing;
     private Transform _playerWhenSeen;
-    
+    private bool waitNow;
+    private RaycastHit _lookForWalls;
+    private Vector3 waitPoint;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -38,7 +40,7 @@ public class RandomMovement : MonoBehaviour
     private void Update()
     {
         // Check if player is within chase range
-        if (Vector3.Distance(_playerWhenSeen.position, transform.position) < enemyChaseRange)
+        if (Vector3.Distance(_playerWhenSeen.position, transform.position) < enemyChaseRange! && Physics.Linecast(transform.position, _playerWhenSeen.position, out _lookForWalls))
         {
             isChasing = true;
             enemyAgent.speed = enemyChaseSpeed;
@@ -51,21 +53,30 @@ public class RandomMovement : MonoBehaviour
             // Check if enemy is done moving
             if (enemyAgent.remainingDistance <= enemyAgent.stoppingDistance && !enemyAgent.pathPending)
             {
+                enemyAnimator.SetBool("Walk", true);
+                enemyAnimator.SetBool("Run", false);
                 // Only move if not already chasing
-                if (!isChasing)
+                if (!isChasing && !waitNow)
                 {
-                    Vector3 point;
-                    if (RandomPoint(centrePoint.position, enemyRange, out point))
+                    if (RandomPoint(centrePoint.position, enemyRange, out waitPoint))
                     {
-                        Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                        Debug.DrawRay(waitPoint, Vector3.up, Color.blue, 1.0f);
                         enemyAnimator.SetBool("Walk", true);
                         enemyAnimator.SetBool("Run", false);
-                        enemyAgent.SetDestination(point);
+                        enemyAgent.SetDestination(waitPoint);
+                        enemyAgent.speed = 3.5f;
+                        waitNow = true;
                     }
                 }
             }
             else
             {
+                if (Vector3.Distance(transform.position, waitPoint) < 0.1f)
+                {
+                    enemyAnimator.SetBool("Walk", false);
+                    enemyAnimator.SetBool("LookAround", true);
+                    waitNow = false;
+                }
                 isChasing = false;
             }
         }
